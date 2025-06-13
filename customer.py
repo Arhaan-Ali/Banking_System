f0a9c6dd88fe712057042049b48b3b9fd5c850ac
Arhaan <aliarhaan761@gmail.com>
2025-06-14 02:10:10 +0530

    customer class push

---
+++ customer.py
@@ -0,0 +1,9 @@
+class Customer :
+    def __init__(self, customer_id, name, address):
+        self.customer_id = customer_id
+        self.name = name
+        self.address = address
+
+
+    def AddorRemoveAcc(self, _account_numbers):
+        pass
