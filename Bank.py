09e090a211d6db23ce77545d18e89e30e9362d08
Arhaan <aliarhaan761@gmail.com>
2025-06-14 02:15:39 +0530

    bank class push

---
+++ Bank.py
@@ -0,0 +1,14 @@
+class Bank:
+    def __init__(self, customer_file = 'customers.json' , account_file = 'accounts.json'):
+        self.customer_file = customer_file
+        self.account_file = account_file
+
+    def _load_data(self):
+        pass
+
+    def _save_data(self):
+        pass
+
+    def AddorRemoveAcc(self):
+        pass
+
