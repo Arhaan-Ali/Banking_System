05b7678b64a854c9f8ce70267d8687c4aa58436c
Arhaan <aliarhaan761@gmail.com>
2025-06-14 10:25:12 +0530

    structural changes

--- account.py
+++ account.py
@@ -5,22 +5,56 @@ class Account(ABC) :
         self.initial_balance = initial_balance
         self._account_holder_id= _account_holder_id
 
-    def transactions(self):
+    @abstractmethod
+    def deposit(self, amount):
         pass
 
+    @abstractmethod
+    def withdraw(self, amount):
+        pass
+
+    def display_details(self):
+        print(f"Acc No: {self._account_number},Balance: ${self.initial_balance}")
+
+    def to_dict(self):
+        return {
+        "account_number": self._account_number,
+        "balance": self.initial_balance,
+        "account_holder_id": self._account_holder_id
+    }
 
 class SavingsAccount(Account):
     def __init__(self, _account_number, _account_holder_id, initial_balance = 0.0, interest_rate = 0.01):
         super().__init__(_account_number, _account_holder_id, initial_balance)
         self._interest_rate = interest_rate
 
-    def transactions(self):
-        pass
+    def deposit(self, amount):
+        if amount > 0 :
+            self.initial_balance += amount
+            return True
+        return  False
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.initial_balance:
+            self.initial_balance -= amount
+            return True
+        return False
+
+
 
 class CheckingAccount(Account):
-    def __init__(self, _account_number, _account_holder_id, initial_balance = 0.0, overdraft_limits = 0.0)
+    def __init__(self, _account_number, _account_holder_id, initial_balance = 0.0, overdraft_limits = 0.0):
         super().__init__(_account_number, _account_holder_id, initial_balance)
         self.overdraft_limit = overdraft_limits
 
-    def transactions(self):
-        pass
+    def deposit(self, amount):
+        if amount > 0 :
+            self.initial_balance += amount
+            return True
+        return  False
+
+    def withdraw(self, amount):
+        if 0 < amount and  (self.initial_balance - amount) >= self.overdraft_limit :
+            self.initial_balance -= amount
+            return True
+        return False
---
+++ main.py
@@ -0,0 +1,13 @@
+import Bank
+class Main:
+    def main(self):
+        bank = Bank()
+        while True:
+            print("1. Add Customer\n2. Create Account\n3. Deposit\n4. Exit")
+            choice = input("Enter your choice")
+
+            if choice == '1':
+                pass
+
+            elif choice == '4':
+                break
